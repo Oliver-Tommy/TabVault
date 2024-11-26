e5c6cf48f2454d6f396d4a7bf987c5053233ae43
Thomas Oliver <oliver.tommy@icloud.com>
2024-11-26 09:34:54 +0000

    Add summernote for admin page features

--- tab/admin.py
+++ tab/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import Tab, Review
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Tab)
+class TabAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'created_at')
+    search_fields = ['title']
+    list_filter = ('difficulty', 'genre',)
+    prepopulated_fields = {'slug': ('title',)}
+
 
 # Register your models here.
-admin.site.register(Tab)
+
 admin.site.register(Review)
