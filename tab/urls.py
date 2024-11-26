08d2237670904c7dc84614f7c3e69401cd64b732
Thomas Oliver <oliver.tommy@icloud.com>
2024-11-26 14:45:49 +0000

    Add tab_detail view

--- tab/urls.py
+++ tab/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.TabList.as_view(), name='home'),
+    path('<slug:slug>/', views.tab_detail, name='tab_detail'),
 ]
--- tab/views.py
+++ tab/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Tab
 
@@ -8,3 +8,26 @@ class TabList(generic.ListView):
     model = Tab
     template_name = "tab/index.html"
     paginate_by = 6
+
+def tab_detail(request, slug):
+    """
+    Display an individual :model:`tab.Tab`.
+
+    **Context**
+
+    ``tab``
+        An instance of :model:`tab.Tab`.
+
+    **Template:**
+
+    :template:`tab/tab_detail.html`
+    """
+
+    queryset = Tab.objects
+    tab = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "tab/tab_detail.html",
+        {"tab": tab},
+    )
