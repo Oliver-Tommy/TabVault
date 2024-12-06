a46909105ab6585d255a8b1242360eb2bdbb37e2
Thomas Oliver <oliver.tommy@icloud.com>
2024-12-06 18:02:59 +0000

    Add comprehensive comments to views

--- tab/views.py
+++ tab/views.py
@@ -7,13 +7,23 @@ from django.contrib.auth.decorators import login_required
 from .models import Tab, Review
 from .forms import ReviewForm
 
-
-# Create your views here.
-
 class TabList(generic.ListView):
+    """
+    Display a paginated list of all available :model:`tab.Tab`.
+
+    **Context**
+
+    ``tab_list``
+        A queryset of all :model:`tab.Tab` instances, paginated.
+
+    **Template:**
+
+    :template:`tab/index.html`
+    """
     model = Tab
     template_name = "tab/index.html"
     paginate_by = 6
+
 
 def tab_detail(request, slug):
     """
@@ -30,20 +40,25 @@ def tab_detail(request, slug):
     ``review_count``
         The total number of reviews for the tab.
 
+    ``average_rating``
+        The average rating of all reviews for the tab.
+
+    ``full_stars``
+        The number of full stars to display in the rating.
+
+    ``half_star``
+        A boolean indicating whether to display a half-star.
+
     **Template:**
 
     :template:`tab/tab_detail.html`
     """
-
-    # queryset = Tab.objects
     tab = get_object_or_404(Tab, slug=slug)
     reviews = Review.objects.filter(tab=tab).order_by("-created_at")
     review_count = reviews.count()
-
     average_rating = reviews.aggregate(Avg('rating')).get('rating__avg') or 0
     full_stars = int(average_rating)
-    half_star = (average_rating - full_stars) >= 0.5  
-
+    half_star = (average_rating - full_stars) >= 0.5
     url = tab.file.url.replace("http", "https")
 
     if request.method == "POST":
@@ -53,14 +68,9 @@ def tab_detail(request, slug):
             review.user = request.user
             review.tab = tab
             review.save()
-            messages.add_message(
-                request, messages.SUCCESS,
-                'Comment submitted'
-            )
+            messages.add_message(request, messages.SUCCESS, 'Comment submitted')
 
     review_form = ReviewForm()
-
-    # increment view count
     tab.views += 1
     tab.save()
 
@@ -80,14 +90,28 @@ def tab_detail(request, slug):
         },
     )
 
+
 def review_edit(request, slug, review_id):
     """
-    view to edit reviews
+    Edit an existing :model:`tab.Review`.
+
+    **Context**
+
+    ``tab``
+        An instance of :model:`tab.Tab`.
+
+    ``review``
+        The :model:`tab.Review` instance being edited.
+
+    ``review_form``
+        A form instance pre-filled with the review data.
+
+    **Redirect:**
+
+    Redirects to the :view:`tab.tab_detail` view for the tab.
     """
     if request.method == "POST":
-
-        queryset = Tab.objects.filter()
-        tab = get_object_or_404(queryset, slug=slug)
+        tab = get_object_or_404(Tab, slug=slug)
         review = get_object_or_404(Review, pk=review_id)
         review_form = ReviewForm(data=request.POST, instance=review)
 
@@ -101,12 +125,24 @@ def review_edit(request, slug, review_id):
 
     return HttpResponseRedirect(reverse('tab_detail', args=[slug]))
 
+
 def review_delete(request, slug, review_id):
     """
-    view to delete review
+    Delete an existing :model:`tab.Review`.
+
+    **Context**
+
+    ``tab``
+        An instance of :model:`tab.Tab`.
+
+    ``review``
+        The :model:`tab.Review` instance being deleted.
+
+    **Redirect:**
+
+    Redirects to the :view:`tab.tab_detail` view for the tab.
     """
-    queryset = Tab.objects.filter()
-    tab = get_object_or_404(queryset, slug=slug)
+    tab = get_object_or_404(Tab, slug=slug)
     review = get_object_or_404(Review, pk=review_id)
 
     if review.user == request.user:
@@ -117,7 +153,23 @@ def review_delete(request, slug, review_id):
 
     return HttpResponseRedirect(reverse('tab_detail', args=[slug]))
 
+
 def search_tabs(request):
+    """
+    Search for :model:`tab.Tab` instances based on user query.
+
+    **Context**
+
+    ``tabs``
+        A queryset of :model:`tab.Tab` instances matching the query.
+
+    ``query``
+        The search string entered by the user.
+
+    **Template:**
+
+    :template:`tab/search_results.html`
+    """
     query = request.GET.get('q', '')
     if query:
         tabs = Tab.objects.filter(
@@ -127,14 +179,27 @@ def search_tabs(request):
         )
     else:
         tabs = Tab.objects.all()
-    
+
     return render(request, 'tab/search_results.html', {
         'tabs': tabs,
         'query': query
     })
 
+
 @login_required
 def toggle_bookmark(request, tab_id):
+    """
+    Toggle the bookmark status of a :model:`tab.Tab` for the logged-in user.
+
+    **Context**
+
+    ``tab``
+        The :model:`tab.Tab` being bookmarked or unbookmarked.
+
+    **Redirect:**
+
+    Redirects to the :view:`tab.tab_detail` view for the tab.
+    """
     tab = get_object_or_404(Tab, id=tab_id)
     if request.method == 'POST':
         if request.user in tab.bookmarks.all():
@@ -143,9 +208,24 @@ def toggle_bookmark(request, tab_id):
         else:
             tab.bookmarks.add(request.user)
             messages.add_message(request, messages.SUCCESS, 'Tab added to bookmarks!')
+
     return redirect('tab_detail', slug=tab.slug)
+
 
 @login_required
 def bookmarked_tabs(request):
+    """
+    Display a list of tabs bookmarked by the logged-in user.
+
+    **Context**
+
+    ``bookmarks``
+        A queryset of :model:`tab.Tab` instances bookmarked by the user.
+
+    **Template:**
+
+    :template:`tab/bookmarked_tabs.html`
+    """
     bookmarks = request.user.bookmarked_tabs.all()
     return render(request, 'tab/bookmarked_tabs.html', {'bookmarks': bookmarks})
+
